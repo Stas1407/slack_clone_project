c2b01130c3ea927ec9c2e96da47dc417677272cc
Stas1407 <stanislaw.rajm@gmail.com>
2021-02-23 22:24:09 +0100

    Sidebar and header are ready

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import {sidebarItemsData} from '../data/SidebarData'
+import {sidebarChannelsData} from '../data/SidebarData'
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
     return (
@@ -13,6 +16,33 @@ function Sidebar() {
                     <AddCircleOutlineIcon />
                 </NewMessage>
             </WorkspaceContainer>
+            <MainChannels>
+                {
+                    sidebarItemsData.map(item => (
+                        <MainChannelItem>
+                            {item.icon}
+                            {item.text}
+                        </MainChannelItem>
+                    ))
+                }
+            </MainChannels>
+            <ChannelsContainer>
+                <NewChannelContainer>
+                    <div>
+                        Channels
+                    </div>
+                    <AddIcon />
+                </NewChannelContainer>
+                <ChannelsList>
+                    {
+                        sidebarChannelsData.map(item => (
+                            <Channel>
+                               {item.text}
+                            </Channel>
+                        ))
+                    }
+                </ChannelsList>
+            </ChannelsContainer>
         </Container>
     )
 }
@@ -53,5 +83,56 @@ const NewMessage = styled.div`
     svg{
         width: 70%;
         height: 70%;
+    }
+`
+
+const MainChannels = styled.div`
+    padding-top: 20px;
+`
+
+const MainChannelItem = styled.div`
+    color: rgb(188, 171, 188);
+    display: grid;
+    grid-template-columns: 15% auto;
+    height: 28px;
+    align-items: center;
+    padding-left: 19px;
+    padding-top: 5px;
+    cursor: pointer;
+    :hover{
+        background: #350D36;
+    }
+`
+
+const ChannelsContainer = styled.div`
+    color: rgb(188, 171, 188);
+    margin-top: 25px;
+`
+
+const NewChannelContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 20px;
+    padding-left: 19px;
+    padding-right: 19px;
+
+    svg{
+        cursor: pointer;
+    }
+`
+
+const ChannelsList = styled.div`
+    padding-top: 5px;
+`
+
+const Channel = styled.div`
+    display: flex;
+    align-items: center;
+    padding-left: 12%;
+    padding-top: 2px;
+    cursor: pointer;
+    :hover{
+        background: #350D36;
     }
 `
